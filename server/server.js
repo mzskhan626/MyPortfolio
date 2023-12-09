016791e607fda6b444c4b38780db66fcd7637fc8
JKhan01 <jkhan266@gmail.com>
2023-12-09 10:31:34 +0530

    EMail Messages Updated. First Version of Client Server completed.

--- server/server.js
+++ server/server.js
@@ -11,6 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
+// EMail Body for the Potential Client
+let email_body_generator = (customer_name) => {
+  const email_body = `Dear ${customer_name},<br>
+  <p>Thank you for reaching out to me and for your interest in my portfolio. I appreciate the time you've taken to explore my work.</p>
+  <p><strong>I strive to respond to emails within 24-48 hours, and I will make it a priority to review your message as soon as possible.</strong>
+  In the meantime, feel free to explore my portfolio to gain a better understanding of my work. If there are specific details or questions you'd like me to address in my response, please let me know.
+  Looking forward to connecting with you soon!</p>
+  
+  <br><footer>Best regards,,<br>Mohammed Khan<br>Full Stack Developer | CA, USA.</footer>`;
+
+  return email_body
+};
+
 // Your route to check server status
 app.get("/check-status", (req, res) => {
   // Respond immediately to the client
@@ -32,21 +45,40 @@ app.post("/send-email", (req, res) => {
     },
   });
 
-  const mailOptions = {
+  const mailOptionsNotification = {
+    from: "jkhan266@gmail.com", // replace with your email
+    to: "jkhan266@gmail.com",
+    subject: `${req.body.subject}`,
+    html: `<b>Client Name:</b> ${req.body.name}<br><b>EMail Address:</b> ${req.body.email}<br><b>Message:</b><br> ${req.body.text}<br><br>source:https://mzskhan626.github.io/MyPortfolio/`
+  }
+
+  // Sending Notification EMail to my inbox
+  transporter.sendMail(mailOptionsNotification, (error, info) => {
+    if (error) {
+      console.error(`Error Occured while Sending Notification at your EMail.\n${error}`);
+    } else {
+      console.info("Successfully Sent Notification at Your EMail");
+    }
+  });
+
+  const mailOptionsCustomer = {
     from: "jkhan266@gmail.com", // replace with your email
     to: recipientEmail,
-    subject: `${req.body.subject}`,
-    text: `${req.body.text}`,
+    subject: `Auto Respones: ${req.body.subject} | Mohammed Khan`,
+    // text: email_body_generator(req.body.name),
+    html: email_body_generator(req.body.name)
   };
 
-  // Sending email
-  transporter.sendMail(mailOptions, (error, info) => {
+  // Sending email to customer
+  transporter.sendMail(mailOptionsCustomer, (error, info) => {
     if (error) {
       console.error(error);
-      res.status(500).send("Internal Server Error");
+      // res.status(500).send("Internal Server Error");
+      res.redirect(`${process.env.CLIENT_SERVER}?success=0`);
     } else {
       console.log("Email sent: " + info.response);
-      res.status(200).send("Email sent successfully");
+      // res.status(200).send("Email sent successfully");
+      res.redirect(`${process.env.CLIENT_SERVER}?success=1`);
     }
   });
 });
